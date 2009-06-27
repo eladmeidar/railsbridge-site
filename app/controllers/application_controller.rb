e9fd350b638fe5a7f588d7b64496618ac1ad5590
Mike Gunderloy <MikeG1@larkfarm.com>
2009-06-27 17:53:34 -0500

    Add approved flag to users; must be approved user to work with projects

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -51,6 +51,15 @@ private
     end
   end
 
+  def require_approved_user
+    unless current_user && current_user.approved?
+      store_location
+      flash[:notice] = "You must be logged in to access this page"
+      redirect_to new_user_session_url
+      return false
+    end
+  end
+
   def require_no_user
     if current_user
       store_location
