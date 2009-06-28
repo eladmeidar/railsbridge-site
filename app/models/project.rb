db0953d2b952f044fc09b51987d623ac9db0fb94
Mike Gunderloy <MikeG1@larkfarm.com>
2009-06-28 07:35:18 -0500

    Order project list by name

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,6 @@ class Project < ActiveRecord::Base
   
   has_attached_file :site_image, :styles => { :medium => "300x200>", :thumb => "140x81>" }
   
-  attr_accessible :name, :short_description, :link, :long_description, :team_lead, :team_lead_email
+  attr_accessible :name, :short_description, :link, :long_description, :team_lead, :team_lead_email, :site_image
   
 end
