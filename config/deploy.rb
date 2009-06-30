e8b0becd31675158c665a75dc7d1610ff1ef24d0
Mike Gunderloy <MikeG1@larkfarm.com>
2009-06-30 07:36:35 -0500

    Provide for production database config file

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,7 @@ set :tag_on_deploy, false # turn off deployment tagging, we have our own tagging
 
 set :keep_releases, 6
 after "deploy:update", "deploy:cleanup"
+after "deploy:update", "deploy:link_config"
 
 # directories to preserve between deployments
 set :asset_directories, ['public/system/site_images']
@@ -17,3 +18,10 @@ set :asset_directories, ['public/system/site_images']
 # depend :remote, :gem, "authlogic",            "=2.0.14"
 # depend :remote, :gem, "mislav-will_paginate", "=2.2.3"
 # depend :remote, :gem, "jscruggs-metric_fu",   "=1.0.2"
+
+namespace :deploy do
+  desc "Re-link config files"
+  task :link_config, :roles => :app do
+    run "ln -nsf #{shared_path}/config/database.yml #{current_path}/config/database.yml"
+  end
+end
